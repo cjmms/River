493aa31ff6413e6dcc167b4f4e979a83a301c299
cjmms <brandon.xyw@gmail.com>
2022-03-11 20:56:04 -0800

    link opengl, glfw and glm

--- River/src/main.cpp
+++ River/src/main.cpp
@@ -1,7 +1,43 @@
 
+#include <GL/glew.h>
+
 #include <iostream>
+#include <GLFW/glfw3.h>
+
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <vector>
 
 int main()
 {
-	std::cout << "grgr" << std::endl;
+    /* Initialize the library */
+    if (!glfwInit())
+    {
+        std::cout << "GLFW failed to init." << std::endl;
+        return false;
+    }
+
+    // default screen size
+    int window_width = 1280;
+    int window_height = 960;
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow* window = glfwCreateWindow(window_width, window_height, "Not a mimic!", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        std::cout << "GLFW window failed to init." << std::endl;
+        return false;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        std::cout << "GLEW init error" << std::endl;
+        return false;
+    }
+
+    glm::vec3 v;
+
 }
